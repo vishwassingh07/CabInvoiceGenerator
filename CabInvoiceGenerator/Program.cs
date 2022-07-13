9cdb9e636d9f1bc59f3a18721e8ab3786df21b1d
Vishwas <baghel.vishwas0@gmail.com>
2022-07-13 15:29:10 +0530

    [Vishwas]add: Cab Invoice Generator Program Added

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Cab Invoice Generator\n************************************");
+        }
+    }
+}
